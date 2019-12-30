b1d46dbea5cb155ea21b528ba343637727bf24ed
DanZZZeDanZZZeDanZZZe <jyjyjyj3@gmail.com>
2019-12-30 19:32:51 +0300

    add sort container

--- src/index.js
+++ src/index.js
@@ -5,12 +5,44 @@ class App extends React.Component {
     render () {
         return (
             <main>
-                
+                <div className="selectionMenu"></div>
+                <Container/>
             </main>
         );
     }
 }  
+class Container extends React.Component {
+    render () {
+        return (
+            <div className="container">
+                <SortContainer length={20}/>
+            </div>
+        );
+    }
+}
+class SortContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.length = props.length; 
+    }
+    renderColumns(length) {
+        let arr = Array(length).fill(null);
+        return arr.map((item, index) => 
+            {
+                const divStyle = {
+                    height: "50%",
+                }
+                return <div className="column" style={divStyle} key={"column" + index}></div>
+            });
+    }
+    render () {
+        return (
+            <div className="sortContainer">
+                {this.renderColumns(this.length)}
+            </div>
+        );
+    }
+}
 ReactDOM.render(
-    <header>Демонстрация работы алгоритмов сортировки</header>,
-    <App />,
+    <App/>,
     document.getElementById("root"));
